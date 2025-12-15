3c2a66fb4654ec9b17fcc9fc644da07a495353f7
leder <lederceli2002@gmail.com>
2025-12-15 11:27:12 -0500

    Agrega Header y estructura inicial de WatchMax

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React,{Component} from "react";
+
+class Header extends Component
+{
+  render(){
+    return(
+        <div>
+            <h1>WATCHMAX</h1>
+            <img src="/imagenes/logo.jpeg" alt="logo.WACTHMAX"></img>
+        </div>
+    );
+  }
+}
+
+export default Header
+
